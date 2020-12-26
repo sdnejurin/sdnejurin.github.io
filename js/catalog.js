0cd8db3044bd06b8feb3fcb4eef234f691db26da
sdnejurin <sdnejurin@mail.ru>
2020-12-26 14:22:21 +0300

    cw

--- js/catalog.js
+++ js/catalog.js
@@ -18,3 +18,29 @@ document.addEventListener('DOMContentLoaded', () => {
 		});
 	};
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+	let catCon = document.querySelector('.catalog__container');
+	let dates = document.querySelector('.dates');
+	let datesPainter = document.querySelectorAll('.dates__painter');
+
+	if (dates) {
+		catCon.addEventListener('click', (e) => {
+			let clickedElement = e.target.closest('.dates__painter');
+			let currentImg = clickedElement.closest('.catalog__bottom').querySelector('.painter__img');
+			let currentSubtitle = clickedElement.closest('.catalog__bottom').querySelector('.painter__subtitle');
+			let currentDate = clickedElement.closest('.catalog__bottom').querySelector('.painter__date');
+			let currentDesc = clickedElement.closest('.catalog__bottom').querySelector('.painter__desc');
+
+			if (clickedElement) {
+
+				datesPainter.forEach(el => {el.classList.remove('dates__painter--is-active')});
+				clickedElement.classList.add('dates__painter--is-active');
+				currentImg.setAttribute('src', e.target.dataset.painterSrc);
+				currentSubtitle.innerHTML = clickedElement.textContent;
+				currentDate.innerHTML = clickedElement.dataset.painterDate;
+				currentDesc.innerHTML = clickedElement.nextElementSibling.textContent;
+			}
+		})
+	}
+});
