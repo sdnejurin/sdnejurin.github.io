a1b9c87ae32c0d9bea19dcefdadb27c0bee0145c
sdnejurin <sdnejurin@mail.ru>
2021-01-15 22:19:53 +0300

    cw

--- js/swiper.js
+++ js/swiper.js
@@ -1,6 +1,7 @@
 const slideHero = document.querySelector('.swiper-container-hero');
 const slideGallery = document.querySelector('.swiper-container-gallery');
 const slidePublications = document.querySelector('.swiper-container-publications');
+const slideProjects = document.querySelector('.swiper-container-projects');
 
 
 let mySwiperHero = new Swiper(slideHero, {
@@ -19,8 +20,8 @@ let mySwiperGallery = new Swiper(slideGallery, {
 	slidesPerGroup: 3,
 	spaceBetween: 50,
 	navigation: {
-    nextEl: '.gallery-nav__btn--next',
-    prevEl: '.gallery-nav__btn--prev',
+    nextEl: '.gallery__btn--next',
+    prevEl: '.gallery__btn--prev',
 	},
 	pagination: {
     el: '.swiper-pagination',
@@ -40,4 +41,14 @@ let mySwiperPublications = new Swiper(slidePublications, {
     el: '.swiper-pagination',
     type: 'fraction',
   },
+});
+
+let mySwiperProjects = new Swiper(slideProjects, {
+	slidesPerView: 3,
+	slidesPerGroup: 3,
+	spaceBetween: 50,
+	navigation: {
+    nextEl: '.partners-nav__btn--next',
+    prevEl: '.partners-nav__btn--prev',
+	},
 });
