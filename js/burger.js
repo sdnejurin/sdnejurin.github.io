3b3644ba3869f424b17ed681a4dbcf508be649ca
sdnejurin <sdnejurin@mail.ru>
2021-02-17 18:16:17 +0300

    cw

--- js/burger.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-	document.querySelector('.burger-btn').addEventListener('click', function() {
-		let menuActive = document.querySelector('.nav-top');
-		let burgerActive = document.querySelector('.burger-btn');
-
-		menuActive.classList.toggle('nav-top--active');
-		burgerActive.classList.toggle('burger-btn--active');
-	});
-});
